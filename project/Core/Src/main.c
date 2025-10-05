231c32e19e34bbb212e492a76916bc4b4d8701b2
Quangnguyn4405 <quang.nguyenk2023@hcmut.edu.vn>
2025-10-06 05:35:27 +0700

    Update for EX6

--- project/Core/Src/main.c
+++ project/Core/Src/main.c
@@ -90,7 +90,7 @@ int main(void)
       GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 |
       GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11 |
       GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15,
-      GPIO_PIN_SET);
+      GPIO_PIN_RESET);
 
   /* USER CODE END 2 */
 
@@ -105,7 +105,6 @@ int main(void)
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
-
 
 /**
   * @brief System Clock Configuration
